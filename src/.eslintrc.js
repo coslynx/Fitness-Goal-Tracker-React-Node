2e71f5b2c81b760b3a8a558b26493f146686a4ae
CosLynx AI <drixisgod@gmail.com>
2024-10-12 20:48:04 +0530

    generated file: `src/.eslintrc.js`

---
+++ src/.eslintrc.js
@@ -0,0 +1,51 @@
+module.exports = {
+  root: true,
+  extends: [
+    'next',
+    'next/core-web-vitals',
+    'plugin:prettier/recommended',
+    'plugin:storybook/recommended',
+  ],
+  plugins: ['prettier', 'storybook'],
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 100,
+        tabWidth: 2,
+        useTabs: false,
+        semi: true,
+        singleQuote: true,
+        trailingComma: 'all',
+        bracketSpacing: true,
+        jsxBracketSameLine: false,
+        arrowParens: 'always',
+        endOfLine: 'lf',
+        proseWrap: 'preserve',
+      },
+    ],
+  },
+};
